300c657e159828640761033fc3b6a5b1ea014afa
Brandon R. Stoner <monokrome@limpidtech.com>
2013-04-21 00:16:49 -0700

    Added comments to buffer structure

--- include/buffer.h
+++ include/buffer.h
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 struct buffer {
-  const char *path;
-  FILE *file;
-  int size;
+  const char *path;    // NULL or path to file buffer links to
+  FILE *file;          // File descriptor for file or 0
+  int size;            // Size of the loaded file or -1
 
-  bool modified;
+  bool modified;       // true if the buffer's content differs from it's file
 };
 
 buffer* buffer_create();
